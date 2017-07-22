d80418899c6f98b457f77cd26a566237bb3a1b5b
clucle <wjdenwls123@gmail.com>
2017-07-22 18:27:02 +0900

    feat: room design 1

---
+++ routes.js
@@ -0,0 +1,12 @@
+
+module.exports = function(app){
+
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/client/public/game.html');
+    });
+
+    app.get('/room', function(req, res) {
+        res.sendFile(__dirname + '/client/public/room.html');
+    });
+
+}
